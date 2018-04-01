69a75caaebb8db184e178a4511802e3931c0e3d8
4b11s <meps123321@gmail.com>
2018-04-01 17:36:08 +0300

    Preparing for the server

--- database.py
+++ database.py
@@ -1,25 +1,6 @@
 import sqlite3
 import random
 
-#def register(id_tg,id_vk):
-#    try:
-#        if  id_vk == 0:
-#            conn = sqlite3.connect('db/maindb.db')
-#            c = conn.cursor()
-#            c.execute("INSERT INTO users (id_tg) VALUES ('{0}')".format(id_tg))
-#            conn.commit()
-#            c.close()
-#    except:
-#        print("Ошибка №2")
-#        main.bot.send_message(id_tg, "Неизвестная ошибка, возможно вы уже зарегестрированы,\n"
-#                                     "эсли это так, не переживайте")
-#
-#        conn = sqlite3.connect('db/maindb.db')
-#        c = conn.cursor()
-#        c.execute("INSERT INTO users (id_vk) VALUES ('{0}')".format(id_tg))
-#        conn.commit()
-#        c.close()
-
 
 def getJoke():
     conn = sqlite3.connect('db/maindb.db')
